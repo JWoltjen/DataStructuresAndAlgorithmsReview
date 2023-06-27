8aea898e1af31fe0ee65f295e358d83d1050706f
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2023-06-27 09:24:16 -0500

    Add project files.

---
+++ DataStructuresAndAlgorithms/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using DataStructuresAndAlgorithms.ADT;
+
+/*
+ * Big O Notation
+ * 
+ * O(n^2) horrible
+ * 
+ * O(n) fair
+ * 
+ * O(1) - "Constant"  Excellent 
+ * Assignment var test = 0;
+ * Declarations var test;
+ * Arithmetic 2+2;
+ * Comparison 2 > 1;
+ * Accessing Element array[1];
+ * Calling Function someFunction();
+ */
+
+Pokemon squirtle = new Pokemon();
